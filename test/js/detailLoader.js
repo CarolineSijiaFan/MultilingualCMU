fac90aded3f560e589d560c4f61777f43b1b19e1
Sijia Fan <79704029+CarolineSijiaFan@users.noreply.github.com>
2025-08-31 22:38:03 -0400

    Update detailLoader.js

--- test/js/detailLoader.js
+++ test/js/detailLoader.js
@@ -18,7 +18,7 @@ class TourDetailLoader {
         
         // Configuration
         this.config = {
-            BASE_PATH: 'content',
+            BASE_PATH: '/MultilingualCMU/test/content',
             DEFAULT_LANGUAGE: 'zh',
             SUPPORTED_LANGUAGES: ['en', 'zh'],
             
